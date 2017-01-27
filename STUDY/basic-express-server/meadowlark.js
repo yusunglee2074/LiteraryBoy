66cdad4b4b1dd58956848e296905a68ad8d6e02d
yusunglee2074 <lys0333@gmail.com>
2017-01-27 16:01:42 +0900

    라우트를 수정해서 랜덤으로 리스트 항목 뽑아서 템플릿으로 보내주는 함수 실습

--- STUDY/basic-express-server/meadowlark.js
+++ STUDY/basic-express-server/meadowlark.js
@@ -14,8 +14,20 @@ app.set('port', process.env.PORT || 3000);
 app.get('/', function(req, res){
 		res.render('home');
 });
+
+// 오늘의 운세 리스트
+var fortunes = [
+	'운이 좋당 ㅋㅋ',
+	'운이 좋지 못해',
+	'운이 진짜 좋아',
+	'집밖에 나가지 말 것',
+	'연애운이 100점',
+	'연애운이 0점',
+];
 app.get('/about', function(req, res){
-		res.render('about');
+		var randomFortune = 
+		    fortunes[Math.floor(Math.random() * fortunes.length)];
+		res.render('about', { fortune: randomFortune });
 });
 
 // 커스템 500페이지
