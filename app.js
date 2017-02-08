401e0f0b2c2f1ba86ab58b4df0785ba90937929a
jh.kim@core-a.org <jazz.k@kakaocorp.com>
2017-02-08 15:34:03 +0900

    테스트 api 추가

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var apiV1 = require('./routes/api_v1');
 
 var app = express();
 // Express 함수 "express()"를 호출해 변수 app에 담는다.
@@ -30,24 +31,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/api/v1', apiV1);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
->>>>>>> feature/express-init
