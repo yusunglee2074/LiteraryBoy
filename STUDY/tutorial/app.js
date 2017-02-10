839aa1c535a60f35b51222d6314b164397465cba
yusunglee2074 <lys0333@gmail.com>
2017-02-10 12:17:39 +0900

    로컬 postgresql 설치후 sequelize 통해 연결, 기본 테이블칼럼 생성

--- STUDY/tutorial/app.js
+++ STUDY/tutorial/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var Sequelize = require('sequelize');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -24,6 +25,57 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+
+// 데이터베이스 설정
+
+
+
+var sequelize = new Sequelize('tutorial', 'postgres', '3030', {
+  host: 'localhost',
+  dialect: 'postgres',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+  define: {
+    timestamps: false // true by default
+  }
+
+});
+var User = sequelize.define('user', {}); // timestamps is false by default
+var Post = sequelize.define('post', {}, {
+  timestamps: true // timestamps will now be true
+});
+
+var User = sequelize.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
+  },
+  lastName: {
+    type: Sequelize.STRING
+  }
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
+
+User.sync().then(function () {
+  // Table created
+  return User.create({
+    firstName: '유성',
+    lastName: '이유성'
+  });
+});
+
+
+User.findOne().then(function (user) {
+	console.log('유성이는 배고프다');
+    console.log(user.get('firstName'));
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
