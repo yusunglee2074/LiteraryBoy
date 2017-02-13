04de2f65fb26ceaae26f6cdffc4160296f4a28ce
yusunglee2074 <lys0333@gmail.com>
2017-02-13 18:15:16 +0900

    모델/index.js 파일에서 각각의 파일을 모아 export
    참조 사이트 : http://totuworld.github.io/2015/08/07/sequelize/

--- models/index.js
+++ models/index.js
@@ -12,5 +12,31 @@ var sequelize = new Sequelize('book', 'postgres', '3030', {
 	},
 });
 
+// 각각의 모델들을 db변수에 넣어서 export한다.
+
+const fs = require('fs');
+const path = require('path');
+
+let db = {};
+
+fs
+    .readdirSync(__dirname)
+	.filter(function(file) {
+		return (file.indexOf(".") !== 0) && (file !== 'index.js');
+	})
+	.forEach(function(file) {
+		var model = sequelize.import(path.join(__dirname, file));
+		db[model.name] = model;
+	});
+
+Object.keys(db).forEach(function(modelName) {
+	if ("associate" in db[modelName]) {
+		db[modeName].associate(db);
+	}
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 module.exports = sequelize;
+module.exports = db;
