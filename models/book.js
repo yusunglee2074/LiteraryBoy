36c6dc7037a260c061ee053fd43392bec197cdf6
jihoon_kim <jh.kim@core-a.org>
2017-02-19 01:34:44 +0900

    book 모델 수정

--- models/book.js
+++ models/book.js
@@ -5,31 +5,14 @@ module.exports = function(sequelize, DataTypes) {
 			allowNull: false,
 			field: 'title',
 		},
-		page: {
-			type: DataTypes.INTEGER,
-			field: 'page',
-		},
-		booksummary: {
-			type: DataTypes.TEXT,
-			field: 'summary',
-		},
-		tableofcontents: {
-			type: DataTypes.TEXT,
-			field: 'contents',
-		},
-		author: {
+		isbn13: {
 			type: DataTypes.STRING,
-			field: 'author',
+			field: 'isbn13',
 		},
-		publisher: {
-			type: DataTypes.STRING,
-			field: 'publisher',
-		},
-		thumbnailimage: {
-			type: DataTypes.STRING,
-			field: 'thumbnail_image_path'
+		raw: {
+			type: DataTypes.JSON,
+			field: 'raw',
 		}
-		} 
-	);
+    });
 	return Book
 };
