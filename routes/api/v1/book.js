4224bf9dfba74f127d0172a85aeef1840177cff6
yusunglee2074 <lys0333@gmail.com>
2017-02-20 02:17:51 +0900

    최신 api명세에 의한 코드 수정
    :ISBN13, header.user_token

--- routes/api/v1/book.js
+++ routes/api/v1/book.js
@@ -40,7 +40,7 @@ router.get('/search', function(req, res, next) {
     });
 });
 
-router.post('/book/add', function(req, res) {
+router.post('/book/:ISBN13', function(req, res) {
 	// if 이미 추가된 책인가? (유저 id와 isbn13의 값으로 검증)
 	    // 추가 된 책이면 추가 됬다고 안내
 	// 추가 된 책이 아닐시 유저 id와 책 ISBN을 받아서 Readbook 오브젝트를 만든다.
@@ -48,7 +48,7 @@ router.post('/book/add', function(req, res) {
 		// 실제코드
 		/*
 		"where": {
-			"isbn13": req.query.isbn
+			"isbn13": req.params['ISBN13']
 		*/
 		// 개발 임의 코드
 		"where": {
@@ -59,7 +59,7 @@ router.post('/book/add', function(req, res) {
 			// 유저의 토큰으로 유저를 찾을 시에
 			/*
 			"where": {
-				"tokenvalue": req.query.tokenvalue
+				"tokenvalue": req.header.user_token
 			*/
 			// 개발 단계 임의 유저
 			"where": {
@@ -82,13 +82,13 @@ router.post('/book/add', function(req, res) {
 
 // api 명세에 읽은책 추가나 삭제에 대한 내용이 없어서 임의로 집어 넣었습니다.
 // 유저 id와 isbn13을 가지고 삭제 한다.
-router.delete('/book/delete', function(req, res) {
+router.delete('/book/:ISBN13', function(req, res) {
 	Model.Readbook.findOne({
 		// 실제 코드
 		/*
 		"where": {
-			"UserId": req.query.tokenvalue,
-			"isbn13": req.query.isbn
+			"UserId": req.header.user_token,
+			"isbn13": req.params['ISBN13']
 		}
 		*/
 		// 테스트용 임의 값
