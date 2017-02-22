a90705a4b7acf857ed309f8b67a714456d11b8d7
jihoon_kim <jh.kim@core-a.org>
2017-02-22 13:55:58 +0900

    쿼타를 초과하면 빈 어레이가 떨어지도록 변경

--- routes/api/v1/book.js
+++ routes/api/v1/book.js
@@ -9,50 +9,56 @@ router.get('/search', function(req, res) {
     console.log(req.query);
     daum.search(req.query.keyword, function(error, response, body) {
         var bodyObject = JSON.parse(body);
-        console.log(bodyObject.channel.item.length);
+        // console.log(bodyObject.channel.item.length);
 
         // send
         // res.send(bodyObject.channel.item);
 
-        if (bodyObject.channel.item.length > 0) {
-            var respArr = [];
-            // save
-            async.each(bodyObject.channel.item, function(item, callback) {
-                // if exist isbn?
-                    // then update or ignore
-                // else
-                    // save
-                respArr.push({
-                    "bookId"       : item.isbn13,
-                    "name"         : item.title,
-                    "author"       : item.author,
-                    "pub_nm"       : item.pub_nm,
-                    "pub_date"     : Math.round(new Date(item.pub_date.substr(0,4), item.pub_date.substr(4,2) - 1, item.pub_date.substr(6,2)).getTime()/1000),
-                    "thumbnailUrl" : item.cover_l_url
-                });
+        if (bodyObject.channel) {
+            if (bodyObject.channel.item.length > 0) {
+                var respArr = [];
+                // save
+                async.each(bodyObject.channel.item, function(item, callback) {
+                    // if exist isbn?
+                        // then update or ignore
+                    // else
+                        // save
+                    respArr.push({
+                        "bookId"       : item.isbn13,
+                        "name"         : item.title,
+                        "author"       : item.author,
+                        "pub_nm"       : item.pub_nm,
+                        "pub_date"     : Math.round(new Date(item.pub_date.substr(0,4), item.pub_date.substr(4,2) - 1, item.pub_date.substr(6,2)).getTime()/1000),
+                        "thumbnailUrl" : item.cover_l_url
+                    });
 
-                /*
-                Model.Book.create({
-                    "title": item.title,
-                    "isbn13": item.isbn13,
-                    "raw": JSON.stringify(item)
-                // }).then(function() {
-                })
-                 */
+                    /*
+                    Model.Book.create({
+                        "title": item.title,
+                        "isbn13": item.isbn13,
+                        "raw": JSON.stringify(item)
+                    // }).then(function() {
+                    })
+                     */
 
-                callback(null);
-            }, function() {
-                res.send({
-                    "message": {
-                        "result": {
-                            "bookList": {
-                                "books": respArr
+                    callback(null);
+                }, function() {
+                    res.send({
+                        "message": {
+                            "result": {
+                                "bookList": {
+                                    "books": respArr
+                                }
                             }
                         }
-                    }
+                    });
                 });
-            });
+            } else {
+                console.log(bodyObject);
+                res.send([]);
+            }
         } else {
+            console.log(bodyObject);
             res.send([]);
         }
     });
