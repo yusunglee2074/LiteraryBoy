0303fdd788e1caf4d194881a5d539e6398bceada
yusunglee2074 <lys0333@gmail.com>
2017-02-22 15:58:48 +0900

    코드 읽기 편하게 수정

--- routes/api/v1/post.js
+++ routes/api/v1/post.js
@@ -19,36 +19,78 @@ router.post('/:ISBN13/text', function(req, res) {
 		*/
 		// 개발 임의 코드
 		"where": {
-			"id": "1"
+			"id": "1",
 			"UserId": req.head.token_value
 		}
-		}).then(function(book) {
-			Model.Post.create({
-				"content": req.body.content,
-				"likecount": 0,
-				"imagepath": null,
-				"theme": req.body.theme,
-				"page": req.body.page, 
-				//if 해쉬태그가 있다면...?
-				// thorugh 테이블이 있다면 생성방법에 대해 검색해봐야함.
-				"UserId": book.UserId,
-				"BookId": book.isbn13,
+	}).then(function(book) {
+		Model.Post.create({
+			"content": req.body.content,
+			"likecount": 0,
+			"imagepath": null,
+			"theme": req.body.theme,
+			"page": req.body.page, 
+			//if 해쉬태그가 있다면...?
+			// thorugh 테이블이 있다면 생성방법에 대해 검색해봐야함.
+			"UserId": book.UserId,
+			"BookId": book.isbn13,
 
-			}).then(function(post) {
-                res.send({
-                    "message": {
-                        "result": {
-                            "Post": post 
-						 }
+		})
+	}).then(function(post) {
+			res.send({
+				"message": {
+					"result": {
+						"Post": post 
 					 }
+				 }
 		});
 	});
 });
 
-router.delete('/:postid', function(req, res) {
+router.post('/:ISBN13/iamge', function(req, res) {
+	var hashtag = [];
+	hashtag.push(req.body.hashtag);
+	// 해쉬태그를 # 마다 쪼개서 딕셔너리로 만든다.
+	var hashdict = {};
+	Model.Readbook.findOne({
+		// 실제코드
+		/*
+		"where": {
+			"isbn13": req.params['ISBN13']
+		*/
+		// 개발 임의 코드
+		"where": {
+			"id": "1",
+			"UserId": req.head.token_value
+		}
+	}).then(function(book) {
+		Model.Post.create({
+			"content": req.body.content,
+			"likecount": 0,
+			// 이미지 처리 코드 추가해야함!!
+			"imagepath": req.file.image,
+			"theme": req.body.theme,
+			"page": req.body.page, 
+			//if 해쉬태그가 있다면...?
+			// thorugh 테이블이 있다면 생성방법에 대해 검색해봐야함.
+			"UserId": book.UserId,
+			"BookId": book.isbn13,
+
+		})
+	}).then(function(post) {
+			res.send({
+				"message": {
+					"result": {
+						"Post": post 
+					 }
+				 }
+		});
+	});
+});
+
+router.delete('/:ISBN13/:postId', function(req, res) {
 	Model.Post.findOne({
 		"where": {
-			"UserId": req.head.token_value,
+			"ReadbookId": req.params['ISBN13']
 			"id": req.params['postid']
 		}
 	}).then(function(post) {
@@ -61,13 +103,11 @@ router.delete('/:postid', function(req, res) {
 	});
 });
 
-router.get('/all', function(req, res) {
-	// 자신의 모든 포스트 출력
-	// if 책 id도 같이 온다면 해당 책 id의 자신의 포스트만 출력 
-	// if 유저 토큰이 빈값, 책 id만 온다면 해당 책 포스트만 모두 출력
+router.get('/:ISBN13', function(req, res) {
+	// 지금은 책에 대한 모든 코멘트를 가져오며, 추후 scope에 따른 if문 돌려야함.
 	Model.Post.findAll({
 		"where": {
-			"UserId": req.head.token_value
+			"ReadbookId": req.params['ISBN13']
 		}
 	}).then(function(post) {
 		res.send({
@@ -95,12 +135,13 @@ router.put('/:postid', function(req, res) {
 			// 이미지 처리 방법을 아직 모른다.
 			imagepath: req.body.image,
 			theme: req.body.theme
-		}).then(function() {
-			res.send({
-				"message": {
-					"result": {
-						"post": {
-							"posts": post 
+		});
+	}).then(function() {
+		res.send({
+			"message": {
+				"result": {
+					"post": {
+						"posts": post 
 						 }
 					}
 				}
