5163115db9d55a0c6ca4ad5d808c9ad84b777f49
jihoon_kim <jh.kim@core-a.org>
2017-02-23 22:26:05 +0900

    url 변경

--- routes/api/v1/post.js
+++ routes/api/v1/post.js
@@ -6,7 +6,7 @@ var multer = require('multer');
 var upload = multer({ dest: 'upload/' })
 
 
-router.post('/:isbn13/image', upload.single('file'), function (req, res, next) {
+router.post('/:isbn13/image/add', upload.single('file'), function (req, res, next) {
     console.log(req.file);
     res.send("http://jangdock.cafe24.com:3011/image/" + req.file.filename);
 });
