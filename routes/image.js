485744335b78539899d488e2c63866f52091c118
jihoon_kim <jh.kim@core-a.org>
2017-02-23 22:43:51 +0900

    url이 여기 있어야 할듯

--- routes/image.js
+++ routes/image.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({ dest: 'upload/' })
 var path = require('path');
 
 /* GET home page. */
@@ -9,5 +11,13 @@ router.get('/:name', function(req, res, next) {
     console.log(__dirname + "/../upload/" + req.params['name']);
     res.sendFile(path.resolve(__dirname + "/../upload/" + req.params['name']), {headers: {'Content-Type': 'image/jpeg'}});
 });
+
+
+
+router.post('/add', upload.single('file'), function (req, res, next) {
+    console.log(req.file);
+    res.send("http://jangdock.cafe24.com:3011/image/" + req.file.filename);
+});
+
 
 module.exports = router;
