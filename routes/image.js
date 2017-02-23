f4bd7c6a5af4821d9ffe4b4f9c04c5e5a2af7adf
Jihoon_Kim <jh.kim@core-a.org>
2017-02-24 02:09:22 +0900

    하드코딩 된 hostname 변경

--- routes/image.js
+++ routes/image.js
@@ -3,20 +3,22 @@ var router = express.Router();
 var multer = require('multer');
 var upload = multer({ dest: 'upload/' })
 var path = require('path');
+var appConfig = require('./config/app.json')[process.env.NODE_ENV];
 
 /* GET home page. */
 router.get('/:name', function(req, res, next) {
-    console.log(req.params['name']);
+    // console.log(req.params['name']);
 
-    console.log(__dirname + "/../upload/" + req.params['name']);
+    // console.log(__dirname + "/../upload/" + req.params['name']);
+    // TODO : content type mime 읽어서 반환하거나, 업로드 받을때, content type 별로 분류해놓아야 할듯.
     res.sendFile(path.resolve(__dirname + "/../upload/" + req.params['name']), {headers: {'Content-Type': 'image/jpeg'}});
 });
 
 
 
 router.post('/add', upload.single('file'), function (req, res, next) {
-    console.log(req.file);
-    res.send("http://jangdock.cafe24.com:3011/image/" + req.file.filename);
+    // console.log(req.file);
+    res.send("http://" + appConfig.hostname + ":" + appConfig.port + "/image/" + req.file.filename);
 });
 
 
