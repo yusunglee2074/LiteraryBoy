8efa8866440ee3a5ac99b2834bbcae58dc2a9656
yusunglee2074 <lys0333@gmail.com>
2017-02-25 14:20:34 +0900

    페이지정보를 알라딘 api에서 가져와서 리드북에 담음

--- routes/api/v1/book.js
+++ routes/api/v1/book.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var async = require('async');
 var daum = require('../../../util/daum');
+var aladin = require('../../../util/add_page_with_aladin');
 var router = express.Router();
 var Model = require('../../../models');
 var sequelize = require('sequelize')
@@ -72,10 +73,14 @@ router.post('/:ISBN13', function(req, res) {
             "isbn13": req.params['ISBN13']
         }
     }).then(function(book) {
-        Model.User.findOne({
-            "where": {
-                "userid": req.get('user_id')
-            }
+        aladin.page_search(req.params['ISBN13'], function(error, response, body) {
+            var jbody = JSON.parse(body);
+            var page = jbody.item.bookinfo.itemPage;
+            
+            Model.User.findOne({
+                "where": {
+                    "userid": req.get('user_id')
+                }
 		}).then(function(user) {
 			Model.Readbook.create({
 				"readstartdate": sequelize.fn('now'),
@@ -83,17 +88,19 @@ router.post('/:ISBN13', function(req, res) {
 				"reading_page": 0,
 				"isbn13": book.get('isbn13'),
 				"BookId": book.get('id'),
-				"UserId": user.get('id')
+				"UserId": user.get('id'),
+                "page": page
 			}).then(function(readbook) {
 				res.send({
 					"message": {
 						"result": {
 							"book": readbook 
-						 }
-					 }
-				});
-			});
-		});
+                             }
+                         }
+                    });
+                });
+            });
+        });
     });
 });
 
