8f6fdafe5cc580403d548fcc8bd58709cf6f5e29
yusunglee2074 <lys0333@gmail.com>
2017-02-26 03:22:02 +0900

    책 검색시 타겟책 어레이 가장 앞으로 땡겨 온다.

--- routes/api/v1/book.js
+++ routes/api/v1/book.js
@@ -6,9 +6,25 @@ var router = express.Router();
 var Model = require('../../../models');
 var ormUtil = require('../../../util/ormUtil');
 var sequelize = require('sequelize')
+var _ = require('underscore')
 
 
 router.get('/search', function(req, res) {
+    var booklist = {
+        '생각하지 않는 사람들': 9788935208647,
+        '미래산업보고서': 9788964358702,
+        '넛지': 9788901093154, 
+        '그랜드 부다페스트 호텔': 9791155810767, 
+        '어린왕자': 9791186634684, 
+        '자존감 수업': 9791186757093, 
+        '테후테후장에 어서 오세요': 9791195491469, 
+        '새는 날아가면서 뒤돌아보지 않는다': 9791186900222, 
+        '어쩌면 별들이 너의 슬픔을 가져갈지도 몰라': 9788959139309, 
+        '왜 나는 너를 사랑하는가': 9788986836240, 
+        '축복': 9788954644501, 
+        '미래의 속도': 9788935211302
+    };
+
     daum.search(req.query.keyword, function(error, response, body) {
         var bodyObject = JSON.parse(body);
 
@@ -43,26 +59,48 @@ router.get('/search', function(req, res) {
                         } else {
                             callback(null);
                         }
-                    }).catch(function (book) {
-                        callback(null);
                     });
                 }, function() {
-                    res.send({
-                        "message": {
-                            "result": {
-                                "bookList": {
-                                    "books": respArr
+                    var hotItem = [];
+                    async.each(_.keys(booklist), function(key, callback) {
+                        if (key.toString().indexOf(req.query.keyword) > -1) {
+                            Model.Book.findOne({"where": {"isbn13": booklist[key].toString() } }).then(function(book) {
+                                if (respArr.length > 0) {
+                                    hotItem.push({
+                                        "bookId"       : book.isbn13,
+                                        "name"         : book.title,
+                                        "author"       : JSON.parse(book.get('raw')).author,
+                                        "page"         : Math.round(Math.random()*100 + Math.random()*300),
+                                        "pub_nm"       : JSON.parse(book.get('raw')).pub_nm,
+                                        "pub_date"       : JSON.parse(book.get('raw')).pub_date,
+                                        "thumbnailUrl"       : JSON.parse(book.get('raw')).cover_l_url
+
+                                    });
+                                    callback(null);
+                                } else {
+                                    callback(null);
+                                }
+                            });
+                        } else {
+                            callback(null);
+                        }
+                    }, function() {
+                        res.send({
+                            "message": {
+                                "result": {
+                                    "bookList": {
+                                        "books": hotItem.concat(_.without(respArr, hotItem))
+                                    }
                                 }
                             }
-                        }
+                        });
                     });
+
                 });
             } else {
-                // console.log(bodyObject);
                 res.send([]);
             }
         } else {
-            // console.log(bodyObject);
             res.send([]);
         }
     });
